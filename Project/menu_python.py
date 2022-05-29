5d02c253cc07e669391a8f954ee9ca53adeb9b28
Мамренко Елизавета <laverati@gmail.com>
2022-05-29 23:48:02 +0300

    added changes

--- Project/menu_python.py
+++ Project/menu_python.py
@@ -18,40 +18,28 @@ line = '-' * 27
 def menu():  # функция, показывающая меню
     menu = 'Меню'
     current_date = str(datetime.now().strftime("%F %T"))  # показывает дату
-    print(current_date.center(len(current_date) + 7))  # выравнивает по центру
-    print(line)
-    print(menu.center(len(menu) + 20))
-    print(line)
+    print(f"{current_date.center(len(current_date) + 7)}\n{line}\n{menu.center(len(menu) + 20)}\n{line}")
     for i in range(len(menu_options)):  # выводит на экране пункты меню
         print(f'{i + 1}. {menu_options[i]}')
 
 
 def system_info():  # функция, выводит системную информацию
     name_1 = 'Системная информация'
-    print(line)
-    print(name_1.center(len(name_1) + 7))
-    print(line)
-    print('Имя операционной системы: ', platform.system())
-    print("Архитектура: " + platform.architecture()[0])
-    print('Имя хоста: ', platform.node())
+    print(f"{line}\n{name_1.center(len(name_1) + 7)}\n{line}\nИмя операционной системы: "
+          f"{platform.system()}\nАрхитектура: {platform.architecture()[0]}\nИмя хоста: {platform.node()}")
 
 
 def memory_info():  # функция, выводит информацию о памяти
     name_2 = 'Информация о памяти'
-    print(line)
-    print(name_2.center(len(name_2) + 7))
-    print(line)
+    print(f"{line}\n{name_2.center(len(name_2) + 7)}\n{line}")
     with open("/proc/meminfo", "r") as f:  # открывает файл на чтение
         lines = f.readlines()              # строки в файле --> список
-    print("     " + lines[0].strip())      # strip удаляет лишние пробелы
-    print("     " + lines[1].strip())
+    print("     " + lines[0].strip() + "\n     " + lines[1].strip())  # strip удаляет лишние пробелы
 
 
 def network_info():  # функция, выводит информацию о сети
     name_3 = 'Сетевая информация'
-    print(line)
-    print(name_3.center(len(name_3) + 7))
-    print(line)
+    print(f"{line}\n{name_3.center(len(name_3) + 7)}\n{line}")
     ip = socket.gethostbyname(socket.getfqdn())  # IP-адрес
     print('IP-адрес: ', ip)
     mac = get_mac()  # MAC-адрес
